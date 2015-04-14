c25f1d138f70376b1d2ab9687fe87ab6f6d42859
Luke_95 <lukedong123@gmail.com>
2015-04-14 23:04:12 +0800

    created task database

--- Main/app/database_task.php
+++ Main/app/database_task.php
@@ -1,11 +1,18 @@
-<? php
+<?php
 	$con=mysql_connect("localhost","root","root");
-	if(!$con) die("Could not connect",mysql_error());	
+	if(!$con) die("Could not connect". mysql_error());
+	if(mysql_query("CREATE DATABASE InfoRobot",$con)){
+		echo "Datebase created";
+	}else echo "Error" . mysql_error();
 	mysql_select_db("InfoRobot",$con);
-	$sql="CREAT TABLE Task{
+	$sql="CREAT TABLE Task(
 		taskname varchar(200),
 		taskdateyear varchar(10),
 		taskdatemonth varchar(10),
 		taskdateday varchar(10),
-		tasktype int(1),
-		"
+		tasktype int
+		)";
+	$c=mysql_query($sql,$con); 
+	if(!$c) echo "FAILED";
+	mysql_close($con);
+?>
