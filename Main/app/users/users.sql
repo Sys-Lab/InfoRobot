c8444f2bd44eec0183abca336e77833d4a950d9c
Luke_95 <lukedong123@gmail.com>
2015-04-21 09:20:23 +0800

    Update users.sql

--- Main/app/users/users.sql
+++ Main/app/users/users.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS 'users'(
-	id int(10) NOT NULL AUTO_INCREMENT,
-	uesrname varchar(20) NOT NULL PRIMARY KEY,
+CREATE TABLE users(
+	id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+	uesrname varchar(20) NOT NULL ,
 	email varchar(50) NOT NULL UNIQUE,
 	password varchar(20) NOT NULL,
 	phone varchar(20) NOT NULL,
 	status tinyint(1) NOT NULL,
 	token varchar(50) NOT NULL,
-	token_exptime int(10) NOT NULL,
-)ENGINE=INNODB  DEFAULT CHARSET=utf8;
+	token_exptime int(10) NOT NULL
+	)
